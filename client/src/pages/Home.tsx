bc76a0f8271c1d79ea6980a54dd05e8e355444cb
larry fotso <fotsoguiffo8@gmail.com>
2024-07-17 00:00:40 -0400

    client -- cors problem fixed

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
+import axios from 'axios';
 import Filter from '../components/Filter';
 import ProjectCards from '../components/ProjectCards';
 
 export default function Home() {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/');
+                console.log('Data from server:', response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div className='flex flex-col items-center h-full'>
             <Filter />
@@ -9,4 +24,3 @@ export default function Home() {
         </div>
     );
 }
-
