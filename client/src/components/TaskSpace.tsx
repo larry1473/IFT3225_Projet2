619dd39853782b0892590f8853edd28a86634d69
MinPro <mbs0722@gmail.com>
2024-07-20 01:47:47 -0400

    Update TaskSpace.tsx

--- client/src/components/TaskSpace.tsx
+++ client/src/components/TaskSpace.tsx
@@ -11,22 +11,22 @@ export default function ProjectSpace() {
 
     return (
         <div className='flex flex-col justify-center p-2'>
-            <form onSubmit={handleAddTaskSubmit} className='flex justify-center gap-x-2 border'>
-                <div className='flex'>
-                <div>
-                    <label htmlFor="">Task name</label>
-                    <input type="text" placeholder='Type a task name'/>
-                </div>
-                <label htmlFor="">Target date</label>
-                <input type="date" />
-                <button className='border p-2 rounded-full'>Add</button>
-                </div>
-            </form>
             <div className='grid grid-cols-3 gap-5 px-5 py-5'>
                 <TaskGroup title="To do" />
                 <TaskGroup title="Doing" />
                 <TaskGroup title="Done" />
             </div>
+            <form onSubmit={handleAddTaskSubmit} className='flex justify-center items-center gap-x-5 border p-2'>
+                <div className='flex flex-col'>
+                    <label htmlFor="">Task name</label>
+                    <input type="text" placeholder='Type a task name'/>
+                </div>
+                <div className='flex flex-col'>
+                    <label htmlFor="">Target date</label>
+                    <input type="date" />
+                </div>
+                <button className='border p-2 rounded-full'>Add task</button>
+            </form>
         </div>
     );
 }
